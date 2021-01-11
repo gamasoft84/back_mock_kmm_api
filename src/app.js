214da220b91af001aa14a4516af5f950a12b2d46
gamasoft84 <richardgama@yahoo.com.mx>
2021-01-11 11:27:03 -0600

    path gateway
    {
        "modelCode": ["G5","DJ"],
        "versionCode": ["S6VE","W5TE"],
        "modelYear": ["2020","2021"]
    }

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-app.use('/api/stock', require('./routes/stock'));
+app.use('/api/gateway/', require('./routes/stock'));
 
 module.exports = app;
